546c7b32ba5aca59c4bfd1dd0ee219493c997915
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2022-10-17 21:53:11 +0300

    feat: integrate eslint pre-commit task

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,13 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: [
+    "next",
+    "turbo",
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
