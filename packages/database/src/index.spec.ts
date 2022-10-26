45dbf8d1001b64812fa9f040c400cdde918cad52
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2022-10-26 22:02:28 +0300

    feat(prisma): add a database package

--- packages/database/src/index.spec.ts
+++ packages/database/src/index.spec.ts
@@ -1,16 +1,3 @@
 // test index.ts
 
 import {expect, it, describe} from 'vitest';
-import {getDefaultUser} from '.';
-
-describe("#getUser", () => {
-  it("should return a user", () => {
-    expect(getDefaultUser()).toMatchInlineSnapshot(`
-          {
-            "id": "1",
-            "name": "Pavel \\"PK\\" Kaminsky",
-            "email" : "test@test.com"
-          }
-        `);
-  });
-})
--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,7 +1,24 @@
-export const getDefaultUser = () => {
-    return {
-        id: "1",
-        name: "Pavel \"PK\" Kaminsky",
-        email: "test@test.com"
-    };
+import {PrismaClient} from '@prisma/client'
+import {faker} from "@faker-js/faker";
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: faker.name.fullName(),
+            email: faker.internet.email(),
+        },
+    })
+    console.log(user)
 }
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
