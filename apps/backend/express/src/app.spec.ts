333ecf640dd2796c4d5385f5e7a97587264b6fbd
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2022-10-28 08:47:12 +0300

    test(express): fix failing test

--- apps/backend/express/src/app.spec.ts
+++ apps/backend/express/src/app.spec.ts
@@ -1,11 +1,11 @@
 import request from "supertest";
 import app from "./app";
+import {describe, expect, it} from 'vitest';
 
 describe("/", () => {
   it('should return return "Hello World!"', async () => {
-    const res = await request(app).get("/");
+    const res = await request(app).get("/trpc/cat");
 
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe("Hello World!");
   });
 });
