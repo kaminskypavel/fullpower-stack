e3feaaa806bf8b05b0af681b103dbec013bbcb16
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2022-10-28 11:50:44 +0300

    fix(eslint): remove next from the custom eslint and add it to next only

---
+++ apps/nextjs/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "extends": [
+        'plugin:@next/next/recommended',
+        "@fullpower-stack/eslint-config-custom",
+    ]
+}
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: [
-    "next",
     "turbo",
     "prettier",
     "eslint:recommended",
