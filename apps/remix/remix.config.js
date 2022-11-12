26cb2152b1b169e11d51c3a778dc28834c39cd4a
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2022-11-12 14:51:42 +0200

    fix(remix): exclude .spec files

--- apps/remix/remix.config.js
+++ apps/remix/remix.config.js
@@ -1,10 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
+
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.{test,spec}.{js,jsx,ts,tsx}"],
   serverDependenciesToBundle: ['@fullpower-stack/ui'],
   watchPaths: [
     './../../packages/ui/**/*.tsx',
