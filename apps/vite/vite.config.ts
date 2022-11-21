e6805f7afd3d3600cbf4e63c46f15c38d83dbfd1
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2022-11-21 16:01:33 +0200

    chore(vite): fix build

--- apps/vite/vite.config.ts
+++ apps/vite/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+// import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +8,8 @@ export default defineConfig({
     // basicSsl(),
     react(),
   ],
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   test: {
     globals: true,
     environment: "happy-dom",
