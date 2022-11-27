896dd48518f35bc50a6fec2e2baeece20135af40
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2022-11-27 19:01:54 +0200

    fix(ui): rename "purge" to "content" as neede for v3

--- packages/ui/tailwind.config.cjs
+++ packages/ui/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
             animation: {
