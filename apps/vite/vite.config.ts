c383545c8ad947f72076a37d5a730c9dbfc5607f
vithano <vithano@gmail.com>
2022-11-30 17:53:04 +0200

    path

--- apps/vite/vite.config.ts
+++ apps/vite/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 // import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
