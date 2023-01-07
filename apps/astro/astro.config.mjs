995f70cfc21007a0f93341a971c50c096c4f8ada
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2023-01-07 19:22:06 +0200

    feat(astro):  add dockerfile and add it to docker-compose

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+// https://docs.astro.build/en/getting-started/
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()]
+});
