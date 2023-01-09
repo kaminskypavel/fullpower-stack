8a735fa85a104144818c346f118a3633d09da154
vithano <vithano@gmail.com>
2023-01-09 22:17:02 +0200

    ci: add playwright html artifact to github action

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "github" : "html",
+  reporter: "html",
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -105,7 +105,7 @@ const config: PlaywrightTestConfig = {
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  outputDir: "test-results/",
+  // outputDir: "test-results/",
 
   /* Run your local dev server before starting the tests */
   // webServer: {
